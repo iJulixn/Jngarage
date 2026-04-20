fdc37e353090f46ebc26c047606cb6aa7a36ac1a
iJulixn <126498282+iJulixn@users.noreply.github.com>
2026-04-19 23:23:08 -0300

    Add files via upload

--- jngarage_full/app.py
+++ jngarage_full/app.py
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body style="background:#0f172a;color:white;text-align:center">
-<h2>💳 Escaneá para pagar</h2>
-<img src="https://api.qrserver.com/v1/create-qr-code/?size=280x280&data={{link}}">
-<br><br>
-<a href="{{link}}" style="color:#22c55e">Abrir pago</a>
-</body>
-</html>
+<form method="POST"><input name="user"><input name="password"><button>Login</button></form>
